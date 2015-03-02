525a970379294d563ecc3cad2b0d9f3202f0eb0e
gchokeen <gchokeen@gmail.com>
2015-03-02 19:44:25 +0530

    grunt dest directory changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,8 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'dist/<%= pkg.name %>.js',
+        dest: 'dist/<%= pkg.name %>.min.js'
       }
     }
   });
